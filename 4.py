283e85df09da687f925bb1a3b5cf83823cc00fc0
LYNETTE WAMBUI <lynettemuraga47@gmail.com>
2025-04-26 19:03:32 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,10 @@
+# Simple Number Swap
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+a, b = b, a
+
+print("After swapping:")
+print("First number:", a)
+print("Second number:", b)
